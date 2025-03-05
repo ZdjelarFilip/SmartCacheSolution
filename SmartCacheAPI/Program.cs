1f916647aaad23ff4d4b99828c98dec18d7bc609
ZdjelarFilip <filip.zdjelar@student.um.si>
2025-03-05 20:28:14 +0100

    Add registration of services in Program.cs

--- SmartCacheAPI/Program.cs
+++ SmartCacheAPI/Program.cs
@@ -1,56 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Orleans.Configuration;
 using SmartCacheAPI.Repositories;
 using SmartCacheAPI.Services;
 using SmartCacheAPI.Settings;
 
-var builder = WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration;
-
-var jwtKey = configuration["Jwt:Key"];
-if (string.IsNullOrEmpty(jwtKey))
+public class Program
 {
-    throw new ArgumentNullException("Jwt:Key", "JWT signing key is missing from configuration.");
-}
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigurationManager configuration = builder.Configuration;
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Register Orleans Silo
-builder.Host.UseOrleans(siloBuilder =>
-{
-    siloBuilder.UseLocalhostClustering()
-        .Configure<ClusterOptions>(options =>
+        // Register Orleans Silo
+        builder.Host.UseOrleans(siloBuilder =>
         {
-            options.ClusterId = "dev";
-            options.ServiceId = "orleans";
-        })
-        .AddAzureBlobGrainStorage("nomnioresource", options =>
-        {
-            options.BlobServiceClient = new Azure.Storage.Blobs.BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
-            options.ContainerName = configuration["AzureStorage:ContainerName"];
-        })
-        .UseInMemoryReminderService();
-});
+            siloBuilder.UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "orleans";
+                })
+                .AddAzureBlobGrainStorage("nomnioresource", options =>
+                {
+                    options.BlobServiceClient = new Azure.Storage.Blobs.BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+                    options.ContainerName = configuration["AzureStorage:ContainerName"];
+                })
+                .UseInMemoryReminderService();
+        });
 
-// Register Services and Controllers
-builder.Services.AddSingleton<IBreachedEmailStorage, AzureBreachedEmailStorage>();
-builder.Services.AddScoped<EmailBreachService>();
-builder.Services.Configure<AzureSettings>(configuration.GetSection("AzureStorage"));
+        // Register Logging
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
+        // Register Services and Controllers
+        builder.Services.AddSingleton<IBreachedEmailStorage, AzureBreachedEmailStorage>();
+        builder.Services.AddScoped<EmailBreachService>();
+        builder.Services.Configure<AzureSettings>(configuration.GetSection("AzureStorage"));
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
